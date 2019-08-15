4e86da9b97de8b05aa7bd3ccd6f6ae3900f8b2ac
tania <tanislavens@yahoo.com>
2019-08-15 16:38:12 -0400

    wip: restructured index file. added actions, components, and reducers folders. built skeletons for initialState and reducer fxns

---
+++ react-redux-project/src/reducers/index.js
@@ -0,0 +1,11 @@
+export const initialState = {
+
+}
+
+export const reducer = (state = initialState, action) => {
+    console.log(action);
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
